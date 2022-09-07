5bcefcd9c0a45e4d6614e1ca596a77d429148766
Miguel López Patricio <ml.patri@gmail.com>
2022-09-07 18:15:01 +0200

    iniciando proyecto microservicios spring

---
+++ user-service/src/main/java/com/tutorial/userservice/model/Bike.java
@@ -0,0 +1,18 @@
+package com.tutorial.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bike{
+
+
+    private String brand;
+    private String model;
+    private int userId;
+
+
+}
---
+++ user-service/src/main/java/com/tutorial/userservice/model/Car.java
@@ -0,0 +1,18 @@
+package com.tutorial.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+
+
+    private String brand;
+    private String model;
+    private int userId;
+
+
+}
